c9e52bf1fe5f57383a823139f92ada4c12ca16d0
Skyler1n <Skyler1n@outlook.com>
2026-07-09 20:49:17 +0800

    fix(leds): enable A690 button backlight
    The A690 button-backlight entry was disabled, leaving the capacitive key LEDs unavailable even after the LCD backlight moved to PWM.
    
    Configure button-backlight as the PMIC button LED sink used by the stock A690 kernel LED table. This is board-local to lcsh75_gb2 and does not affect A60+.
    
    Board: lcsh75_gb2 (Lenovo A690)
    
    Signed-off-by: Skyler1n <Skyler1n@outlook.com>

--- mediatek/custom/lcsh75_gb2/kernel/leds/mt65xx/cust_leds.c
+++ mediatek/custom/lcsh75_gb2/kernel/leds/mt65xx/cust_leds.c
@@ -32,7 +32,7 @@ static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
 	{"blue",              MT65XX_LED_MODE_NONE, -1},
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1},
-	{"button-backlight",  MT65XX_LED_MODE_NONE, -1},
+	{"button-backlight",  MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_BUTTON},
 	{"lcd-backlight",     MT65XX_LED_MODE_PWM, PWM2},
 };
 
